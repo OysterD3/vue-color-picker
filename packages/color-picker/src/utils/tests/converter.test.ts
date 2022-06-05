e123ad97db061843cbc50d3a3ec57afeb91bb88d
Oyster Lee <oysterd3@gmail.com>
2022-06-05 20:26:51 +0800

    ✅ test: add more test case for `convert`

--- packages/color-picker/src/utils/tests/converter.test.ts
+++ packages/color-picker/src/utils/tests/converter.test.ts
@@ -1,4 +1,5 @@
-import { HSVAtoRGBA, RGBAtoHSVA } from "../convert";
+import { expect } from "vitest";
+import { HSLAtoRGBA, HSVAtoRGBA, RGBAtoHSLA, RGBAtoHSVA } from "../convert";
 
 describe("Converter", () => {
   it("should be hsv(240, 1, 1)", () => {
@@ -6,6 +7,15 @@ describe("Converter", () => {
       h: 240,
       s: 100,
       v: 100,
+      a: 1,
+    });
+  });
+
+  it("should be hsv(231, 48.7, 15.3)", () => {
+    expect(RGBAtoHSVA({ r: 20, g: 23, b: 39, a: 1 })).toEqual({
+      h: 231,
+      s: 48.72,
+      v: 15.29,
       a: 1,
     });
   });
@@ -28,5 +38,38 @@ describe("Converter", () => {
         a: 1,
       }),
     ).toEqual({ r: 19, g: 139, b: 23, a: 1 });
+  });
+
+  it("should be hsl(0, 82.61, 4.51)", () => {
+    expect(
+      RGBAtoHSLA({
+        r: 21,
+        g: 2,
+        b: 2,
+        a: 1,
+      }),
+    ).toEqual({ h: 0, s: 82.61, l: 4.51, a: 1 });
+  });
+
+  it("should be hsl(180, 100, 50)", () => {
+    expect(
+      RGBAtoHSLA({
+        r: 0,
+        g: 255,
+        b: 255,
+        a: 1,
+      }),
+    ).toEqual({ h: 180, s: 100, l: 50, a: 1 });
+  });
+
+  it("should be rgb(76, 38, 38)", () => {
+    expect(
+      HSLAtoRGBA({
+        h: 0,
+        s: 33.33,
+        l: 22.35,
+        a: 1,
+      }),
+    ).toEqual({ r: 76, g: 38, b: 38, a: 1 });
   });
 });
